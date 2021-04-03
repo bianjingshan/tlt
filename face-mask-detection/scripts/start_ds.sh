fec6a9130c3565f0530d8413b5399040db542ae1
bianjingshan <bianjingshan@163.com>
2021-04-03 19:29:29 +0800

    ds test fist version

--- face-mask-detection/scripts/start_ds.sh
+++
@@ -1,1 +0,0 @@
-sudo docker run --rm --gpus all --name ds_test --device=/dev/video0 -it -p 8554:8554  -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -v "/home/jia/wxh/tlt-demo":"/tlt-demo" -w /tlt-demo/face-mask-detection/ds_configs nvcr.io/nvidia/deepstream:5.0.1-20.09-samples /bin/bash
